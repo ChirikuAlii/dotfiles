babd9bdb02a6339ec49ba168cfeba7b7ffb21a07
Ali Rahmat Ismail <chiriku.ali.rhmat@gmail.com>
2025-12-15 06:47:08 +0700

    add config tmux,ghostty,nvim script

---
+++ ghostty/setup-ghostty.sh
@@ -0,0 +1,8 @@
+
+mkdir -p "$HOME/.config/ghostty"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  ln -sf "$DOT/ghostty/config.mac" "$HOME/.config/ghostty/config"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  ln -sf "$DOT/ghostty/config.linux" "$HOME/.config/ghostty/config"
+fi
---
+++ setup.sh
@@ -0,0 +1,5 @@
+export DOT="$HOME/1.Code/2.Areas/dotfiles"
+
+ln -sf "$DOT/nvim" "$HOME/.config/nvim"
+ln -sf "$DOT/tmux/.tmux.conf" "$HOME/.tmux.conf"
+sh "$DOT/ghostty/setup-ghostty.sh"
