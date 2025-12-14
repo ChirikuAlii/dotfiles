bea238979b5c7bb66206345e22e5624a4c881489
Ali Rahmat Ismail <chiriku.ali.rhmat@gmail.com>
2025-12-15 06:01:19 +0700

    add nvim surround

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,19 +1,31 @@
-return { {
-  "nvim-mini/mini.nvim",
+return {
+  {
+    "nvim-mini/mini.nvim",
+    config = function()
+      -- require("mini.surround").setup()
+      require("mini.comment").setup()
+      require("mini.ai").setup()
+
+      require("mini.pairs").setup()
+      require("mini.move").setup()
+      require("mini.bracketed").setup()
+      require("mini.snippets").setup()
+
+      --ini jump ke
+      require("mini.jump").setup()
+
+      -- indent scope bisa select sesuai scope pakai ke visual mode -> ai atau ii
+      require("mini.indentscope").setup()
+    end,
+
+  },
+  "kylechui/nvim-surround",
+  version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
   config = function()
-    -- require("mini.surround").setup()
-    require("mini.comment").setup()
-    require("mini.ai").setup()
+    require("nvim-surround").setup({
+      -- Configuration here, or leave empty to use defaults
+    })
+  end
 
-    require("mini.pairs").setup()
-    require("mini.move").setup()
-    require("mini.bracketed").setup()
-    require("mini.snippets").setup()
-
-    --ini jump ke
-    require("mini.jump").setup()
-
-    -- indent scope bisa select sesuai scope pakai ke visual mode -> ai atau ii
-    require("mini.indentscope").setup()
-  end,
 }
